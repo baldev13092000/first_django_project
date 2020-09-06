5524a88bb0c7b214fbffebf4601da01dba3aaf44
baldev sundarani <baldev.sundarani.100@gmail.com>
2020-09-06 12:37:34 +0530

    'first'

---
+++ first_project/first_app/template_tags/my_extras.py
@@ -0,0 +1,6 @@
+from django import template
+register = template.Library()
+
+def cut(value,arg):
+    return value.replace(arg,'')
+register.filter('cut':cut)
---
+++ first_project/first_app/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from first_app import views
+app_name = 'first_app'
+urlpatterns = [
+    url(r'^index/$',views.index,name = 'index'),
+    url(r'^register/$',views.register,name = 'register'),
+    url(r'^user_login/$',views.user_login,name='user_login'),
+]
